dba7fb4c845207c0cc371f27c879253087f7e9ba
Arek Nowicki <nowickiark@outlook.com>
2019-05-29 22:44:26 +0200

    Simple program with created POJOS for Book and Authors. Additionally displaying data information via ThymeLeafe repo.

---
+++ src/main/java/guru/springframework/spring5webapp/controller/AuthorControler.java
@@ -0,0 +1,28 @@
+package guru.springframework.spring5webapp.controller;
+
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorControler {
+
+    AuthorRepository authorRepository;
+
+    public AuthorControler(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model){
+
+        model.addAttribute("authors",authorRepository.findAll());
+
+        return "authors";
+
+    }
+
+
+
+}
